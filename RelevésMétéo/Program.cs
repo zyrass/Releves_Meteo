e34419e92aad9b4e29008190180c0a97fd737955
Alain Guillon <zyrass@outlook.fr>
2024-02-24 13:32:30 +0100

    Ajout de la solution proposé par l'enseignant

--- RelevésMétéo/Program.cs
+++ RelevésMétéo/Program.cs
@@ -68,13 +68,46 @@
 
     }
 
+    static void AfficherTableauSolutionByCyril()
+    {
+      const string entetes = """
+			Mois | T° min | T° max | Soleil | Pluie (mm)
+		-----------------------------------------------
+		""";
+      Console.WriteLine(entetes);
+
+      string[] lignes = File.ReadAllLines("meteoParis.csv");
+      for (int i = 1; i < lignes.Length; i++)
+      {
+        // Simplifie le format des heures d'ensoleillement
+        string ligne = lignes[i].Replace("h ", "h").Replace("min", "");
+
+        // Récupère les infos de la ligne dans un tableau
+        string[] infos = ligne.Split(';');
+
+        // Transforme les chaînes en nombres
+        if (float.TryParse(infos[2], out float tmin) &&
+          float.TryParse(infos[3], out float tmax) &&
+          float.TryParse(infos[7], out float pluie))
+        {
+          // Affiche la ligne sous la forme souhaitée
+          Console.WriteLine($"{infos[0]}/{infos[1]} | {tmin,6:N1} | {tmax,6:N1} | {infos[6],6} | {pluie,10:N1}");
+        }
+        else
+          Console.WriteLine("Erreur à la ligne {0}", i);
+      }
+    }
+
     static void Main(string[] args)
     {
       // Afficher la liste météorologique en liste
-      // AfficherListe();
+      AfficherListe();
 
       // Afficher la liste météorologique en tableau
       AfficherTableau();
+
+      // Afficher la solution proposé par Cyril
+      AfficherTableauSolutionByCyril();
     }
   }
 }
